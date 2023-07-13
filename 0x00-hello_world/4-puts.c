b7c6bd9e284d0bdb568cc6416b7d33115b32afc0
Sunday_Godwin <gift85210@gmail.com>
2023-07-13 14:55:23 -0700

    print with puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line with put command
+ * Return: 0 (sucess)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
